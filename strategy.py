0673293980c680e188eefbfa7523d2285718d2ad
KhalVa26 <bd1460494@gmail.com>
2026-03-13 21:28:05 +0100

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -25,19 +25,18 @@ def check_signal(df):
     if last["ema50"] > last["ema200"] and 40 < last["rsi"] < 50 and volume_ok:
 
         return {
-            "type": "LONG",
-            "entry": round(price,2),
-            stop = price - atr * 1.5
-            take = price + atr * 3
-        }
-
+    "type": "LONG",
+    "entry": round(price, 2),
+    "stop": round(price - atr * 1.5, 2),
+    "take": round(price + atr * 3, 2)
+}
     if last["ema50"] < last["ema200"] and 50 < last["rsi"] < 60:
 
-        return {
-            "type": "SHORT",
-            "entry": round(price,2),
-            stop = price + atr * 1.5
-            take = price - atr * 3
-        }
+       return {
+    "type": "SHORT",
+    "entry": round(price, 2),
+    "stop": round(price + atr * 1.5, 2),
+    "take": round(price - atr * 3, 2)
+}
 
     return None
