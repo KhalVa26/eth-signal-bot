2c945b4f298987147a0e8b98747f04d492f0293c
KhalVa26 <bd1460494@gmail.com>
2026-03-15 17:14:30 +0100

    Update market.py

--- market.py
+++ market.py
@@ -5,12 +5,10 @@ exchange = ccxt.binance()
 
 def get_ohlcv(symbol, timeframe, limit=200):
     bars = exchange.fetch_ohlcv(symbol, timeframe=timeframe, limit=limit)
-
-df = pd.DataFrame(
-    bars,
-    columns=["time","open","high","low","close","volume"]
-)
-df["time"] = pd.to_datetime(df["time"], unit="ms")  # перетворюємо час
-df.set_index("time", inplace=True)  # ставимо його як індекс
-
+    df = pd.DataFrame(
+        bars,
+        columns=["time", "open", "high", "low", "close", "volume"]
+    )
+    df["time"] = pd.to_datetime(df["time"], unit="ms")
+    df.set_index("time", inplace=True)
     return df
