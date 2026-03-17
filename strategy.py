390e27876dd48839f815032fae3117e34177da56
KhalVa26 <bd1460494@gmail.com>
2026-03-17 20:28:07 +0100

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -1,5 +1,6 @@
-from market import get_ohlcv
 import ta
+from market import get_ohlcv
+
 
 def calculate_indicators(df):
 
@@ -20,35 +21,46 @@ def check_signal(df):
 
     df = calculate_indicators(df)
     last = df.iloc[-1]
-    # ===== 1H TREND =====
-df1h = get_ohlcv("ETH/USDT", "1h")
-
-df1h["ema200"] = ta.trend.ema_indicator(df1h["close"], window=200)
-
-trend = "bull" if df1h.iloc[-1]["close"] > df1h.iloc[-1]["ema200"] else "bear"
 
     atr = last["atr"]
     volume_ok = last["volume"] > last["vol_ma"]
     price = last["close"]
 
-    # LONG
-    if last["ema50"] > last["ema200"] and 35 < last["rsi"] < 55 and volume_ok and trend == "bull":
+    # ===== 1H TREND =====
+    df1h = get_ohlcv("ETH/USDT", "1h")
+
+    df1h["ema200"] = ta.trend.ema_indicator(df1h["close"], window=200)
+
+    trend = "bull" if df1h.iloc[-1]["close"] > df1h.iloc[-1]["ema200"] else "bear"
+
+    # ===== LONG =====
+    if (
+        last["ema50"] > last["ema200"]
+        and 35 < last["rsi"] < 55
+        and volume_ok
+        and trend == "bull"
+    ):
 
         return {
             "type": "LONG",
             "entry": round(price, 2),
             "stop": round(price - atr * 1.5, 2),
-            "take": round(price + atr * 3, 2)
+            "take": round(price + atr * 3, 2),
         }
 
-    # SHORT
-    if last["ema50"] < last["ema200"] and 45 < last["rsi"] < 65 and volume_ok and trend == "bear":
+    # ===== SHORT =====
+    if (
+        last["ema50"] < last["ema200"]
+        and 45 < last["rsi"] < 65
+        and volume_ok
+        and trend == "bear"
+    ):
 
         return {
             "type": "SHORT",
             "entry": round(price, 2),
             "stop": round(price + atr * 1.5, 2),
-            "take": round(price - atr * 3, 2)
+            "take": round(price - atr * 3, 2),
         }
 
     return None
