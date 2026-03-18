b8a51d3baaeae691be481fc0e59b0bb11e2a6cd6
KhalVa26 <bd1460494@gmail.com>
2026-03-18 21:39:13 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,18 @@
 BOT_TOKEN = "8786610017:AAE-DowOMVgp2MpcLJHiGgkUgp2mP2gvn5A"
 CHAT_ID = "8121421612"
 
+# Старе залишаємо (щоб нічого не впало)
 SYMBOL = "ETH/USDT"
+
+# ➕ ДОДАЄМО СПИСОК МОНЕТ
+SYMBOLS = [
+    "BTC/USDT",
+    "ETH/USDT",
+    "SOL/USDT",
+    "BNB/USDT",
+    "ATOM/USDT",
+]
+
 TIMEFRAME = "15m"
 
 CHECK_INTERVAL = 900
